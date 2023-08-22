c4c6997d5db9a8b01471bb0aca670317786a1d45
LeeSinGit <118451245+LeeSinGit@users.noreply.github.com>
2023-08-22 21:15:43 +0300

    try workflow25

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -47,3 +47,38 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(
+            self,
+            email,
+            username,
+            password=None,
+            **extra_fields
+    ):
+        """
+        Создает и сохраняет суперпользователя с указанными данными.
+
+        Args:
+            email (str): Электронная почта пользователя.
+            username (str): Ник пользователя.
+            first_name (str): Имя пользователя.
+            last_name (str): Фамилия пользователя.
+            password (str, optional): Пароль пользователя. Defaults to None.
+            **extra_fields: Дополнительные поля пользователя.
+
+        Returns:
+            User: Созданный суперпользователь.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(
+            email,
+            password,
+            **extra_fields
+        )
