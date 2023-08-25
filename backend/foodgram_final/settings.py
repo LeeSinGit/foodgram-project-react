a81760dea41dce550c512c10d02ff42949df404e
LeeSinGit <118451245+LeeSinGit@users.noreply.github.com>
2023-08-25 16:01:07 +0300

    101

--- backend/foodgram_final/settings.py
+++ backend/foodgram_final/settings.py
@@ -72,7 +72,6 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
-        # 'HOST': 'localhost',
         'PORT': os.getenv('DB_PORT', default=5432),
     }
 }
