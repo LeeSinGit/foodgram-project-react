5900882310282510addefdfe2707e661da674ce1
LeeSinGit <118451245+LeeSinGit@users.noreply.github.com>
2023-08-27 20:25:54 +0300

    rew8

--- backend/baseapp/admin.py
+++ backend/baseapp/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 
-from .models import (
-    Favorite,
-    Ingredient,
-    Recipe,
-    RecipeIngredients,
-    ShoppingCart,
-    Tag,
-)
+from .models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
 
 
 @admin.register(Ingredient)
